0bf680cabaf15732c5713e12a641dad79627a0cb
sam greaves <samgreaves0@gmail.com>
2025-07-13 16:46:55 +0100

    nitial JitPack version

--- updatechecker/build.gradle.kts
+++ updatechecker/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22"
 }
 
+group = "com.github.sgreaves1"
+version = "1.0.0"
+
 android {
     namespace = "com.example.updatechecker"
     compileSdk = 34
